ca9b31c31c9cba2e5ea8bca6b377fffe67b506ce
Thuan Huynh <minhthuan.dev@gmail.com>
2017-05-12 16:34:52 +0700

    [M]

--- Robot/WindowsFormsApp1/Crawler.DTO/SubLinkDTO.cs
+++ Robot/WindowsFormsApp1/Crawler.DTO/SubLinkDTO.cs
@@ -9,10 +9,11 @@ namespace Crawler.DTO
     public class SubLinkDTO
     {
         public string ID { get; set; }
+        public string LinkID { get; set; }
         public string Name { get; set; }
         public string SubLink { get; set; }
         public bool IsRun { get; set; }
-        public string XPathTitel { get; set; }
+        public string XPathTitle { get; set; }
         public string XPathDate { get; set; }
         public string XPatheImage { get; set; }
         public string XPathDescription { get; set; }
